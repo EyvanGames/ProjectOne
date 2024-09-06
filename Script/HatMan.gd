45947037027957dd3b4667635c8967de5f4a5c5a
Tash <aliboyok.75@gmail.com>
2024-09-06 14:18:15 +0330

    converted to godot 4

--- Script/HatMan.gd
+++ Script/HatMan.gd
@@ -1,11 +1,11 @@
-extends KinematicBody
+extends CharacterBody3D
 
-onready var camera_rig = $CameraRig
-onready var camera = $CameraRig/Camera
-onready var anim_node = $Armature/Skeleton/AnimationTree
-export var jump_speed:float = 10.0
+@onready var camera_rig = $CameraRig
+@onready var camera = $CameraRig/Camera3D
+@onready var anim_node = $Armature/Skeleton3D/AnimationTree
+@export var jump_speed:float = 10.0
 var direction = Vector3.ZERO
-var velocity = Vector3.ZERO
+var velociting = Vector3.ZERO
 var gravity:float = 50.0
 var movement_speed:float = 0.0
 var walk_speed:float = 1.5
@@ -13,7 +13,7 @@ var run_speed:float = 5.0
 var acceleration = 6
 
 func _ready():
-	camera_rig.set_as_toplevel(true)
+	camera_rig.set_as_top_level(true)
 
 func _physics_process(delta):
 	run(delta)
@@ -40,16 +40,19 @@ func run(delta):
 		anim_node.set("parameters/move_blend/blend_amount", lerp(anim_node.get("parameters/move_blend/blend_amount") , -1.0, delta * acceleration))
 		movement_speed = 0
 	if direction != Vector3.ZERO and not Input.is_action_pressed("RMB"):
-		look_at(translation + direction, Vector3.UP)
+		look_at(position + direction, Vector3.UP)
 	
-	velocity.x = direction.x * movement_speed
-	velocity.z = direction.z * movement_speed
-	velocity.y -= gravity * delta
-	velocity = move_and_slide(velocity, Vector3.UP)
+	velociting.x = direction.x * movement_speed
+	velociting.z = direction.z * movement_speed
+	velociting.y -= gravity * delta
+	set_velocity(velociting)
+	set_up_direction(Vector3.UP)
+	move_and_slide()
+	velociting = velociting
 
 func jump():
 	if is_on_floor() and Input.is_action_pressed("jump"):
-		velocity.y += jump_speed
+		velociting.y += jump_speed
 		anim_node.set("parameters/jump/active", true)
 		anim_node.set("parameters/guard_transition/current", 1)
 	
@@ -66,4 +69,3 @@ func aim():
 	var to = from + camera.project_ray_normal(mouse_pos) * ray_length
 	var cursor_pos = drop_plane.intersects_ray(from, to)
 	look_at(cursor_pos, Vector3.UP)
-
